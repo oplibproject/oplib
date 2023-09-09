8478b31c0fd9f52dd331d34051e2b365b5cefc3e
oplibproject <144467575+oplibproject@users.noreply.github.com>
2023-09-09 12:28:18 +0200

    Skeleton monitoring and analytics AI tools

---
+++ search.py
@@ -0,0 +1,16 @@
+temp = int(input("please enter temp "))
+if temp > 30:
+    print("its hot")
+elif temp < 30 and temp > 20
+    print("abc")
+elif temp > 1 and temp <20
+    print("its cold")
+
+
+
+
+
+
+
+
+
