460f0151c672365d081f6aa3b044faf924825358
oplibproject <144467575+oplibproject@users.noreply.github.com>
2023-09-09 14:40:22 +0200

    Skeleton code for AI-ML crawlers.py

--- app.py
+++ app.py
@@ -1,8 +1,127 @@
-i = int(input("please enter number"))
-while i >0:
-    print(i * '*')
-    i = i-1
+import scrapy
+import tensorflow as tf
+import sklearn
+from elasticsearch import Elasticsearch, helpers, TransportError
+from prometheus_client import start_http_server, Summary, Gauge
+import h2o
+import redis
+import logging
+import kafka
+from kafka import KafkaProducer
+import asyncio
+from sklearn.pipeline import Pipeline
+import os
+import json
+from concurrent.futures import ThreadPoolExecutor
 
+# Initialize environment variables
+START_URL = os.getenv("START_URL", "http://example.com")
 
+# Initialize Prometheus metrics
+REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
+ACTIVE_SPIDERS = Gauge('active_spiders', 'Number of active spiders')
 
+# Initialize logging
+logging.basicConfig(
+    filename="crawler.log",
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+handler = logging.handlers.RotatingFileHandler("crawler.log", maxBytes=10**6, backupCount=5)
+logging.getLogger().addHandler(handler)
 
+# Initialize H2O
+h2o.init(ip="localhost", port=54321)
+
+# Initialize Elasticsearch
+es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+
+# Initialize Redis for job queuing
+r = redis.Redis(host='localhost', port=6379, db=0)
+
+# Initialize Kafka producer for real-time data streaming
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
+
+# Load pre-trained TensorFlow model
+model = tf.keras.models.load_model('path/to/your/model')
+
+# Create Elasticsearch index if it doesn't exist
+try:
+    es.indices.create(index='my_index')
+except TransportError:
+    pass
+
+class MySpider(scrapy.Spider):
+    name = 'my_spider'
+    
+    def start_requests(self):
+        urls = [START_URL]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse, errback=self.errback)
+    
+    def errback(self, failure):
+        logging.error(f"Error occurred: {failure}")
+        
+    @REQUEST_TIME.time()
+    def parse(self, response):
+        logging.info(f"Processing {response.url}")
+        ACTIVE_SPIDERS.inc()
+
+        # Scrape and preprocess data
+        page_text = response.xpath("//p/text()").getall()
+        
+        # Create machine learning pipeline
+        ml_pipeline = Pipeline([
+            ('tfidf', TfidfVectorizer()),
+            ('scaler', StandardScaler(with_mean=False))
+        ])
+        
+        tfidf_matrix = ml_pipeline.fit_transform(page_text)
+        predictions = model.predict(tfidf_matrix)
+        
+        # Data for Elasticsearch
+        docs = [
+            {
+                '_index': 'my_index',
+                '_type': 'text',
+                '_source': {
+                    'text': text,
+                    'features': feature,
+                    'prediction': prediction
+                }
+            }
+            for text, feature, prediction in zip(page_text, tfidf_matrix.toarray().tolist(), predictions.tolist())
+        ]
+        
+        try:
+            helpers.bulk(es, docs)
+        except Exception as e:
+            logging.error(f"Failed to index document: {e}")
+            
+        # Send data to Kafka topic
+        producer.send('my_topic', value=docs)
+        
+        # Checkpoint in Redis
+        r.set(f"checkpoint:{response.url}", json.dumps(docs))
+
+        ACTIVE_SPIDERS.dec()
+
+if __name__ == "__main__":
+    start_http_server(8000)
+    
+    # Use thread pool for concurrent requests
+    executor = ThreadPoolExecutor(max_workers=50)
+    
+    settings = {
+        'USER_AGENT': 'Mozilla/5.0',
+        'LOG_LEVEL': 'INFO',
+        'DOWNLOAD_DELAY': 0.25,
+        'CONCURRENT_REQUESTS_PER_DOMAIN': 10,
+        'RETRY_ENABLED': True,
+        'RETRY_TIMES': 3,
+        'EXECUTOR': executor
+    }
+    
+    process = CrawlerProcess(settings)
+    process.crawl(MySpider)
+    process.start()
