78b0ac6daafce99e41cd15eff7ef8e2865bc9db2
oplibproject <144467575+oplibproject@users.noreply.github.com>
2023-09-09 16:02:14 +0200

    Update and rename Query skeleto code.py to Ranking Algorithm Skeleton code.py

---
+++ Ranking algorithim/Ranking Algorithm Skeleton code.py
@@ -0,0 +1,82 @@
+import os
+import json
+import logging.config
+import yaml
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from elasticsearch import Elasticsearch, RequestsHttpConnection, AsyncElasticsearch
+import spacy
+import numpy as np
+from transformers import BertTokenizer, BertModel
+import torch
+import asyncio
+from functools import partial
+from prometheus_client import start_http_server, Summary
+from sklearn.ensemble import RandomForestRegressor
+from elasticsearch import Elasticsearch
+from transformers import BertTokenizer, BertModel
+import numpy as np
+from pydantic import BaseModel
+from redis import Redis
+from celery import Celery
+
+# Initialize Redis
+redis = Redis(host='localhost', port=6379, db=0)
+
+# Initialize Celery
+celery_app = Celery('tasks', broker='redis://localhost:6379/0')
+
+# Initialize Elasticsearch
+es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+
+# Initialize BERT model and tokenizer
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+bert_model = BertModel.from_pretrained('bert-base-uncased')
+
+# Initialize spaCy
+nlp = spacy.load("en_core_web_sm")
+
+# Pydantic Model
+class QueryModel(BaseModel):
+    query: str
+
+# AdvancedRanker class
+class AdvancedRanker:
+    def __init__(self):
+        self.random_forest_model = RandomForestRegressor()  # Dummy, to be replaced with a trained model
+
+    def select_model(self, query):
+        # Logic to select the most appropriate model for a query
+        return self.random_forest_model
+    
+    @celery_app.task(rate_limit='10/m')  # Limit to 10 tasks per minute
+    def async_rerank(self, query):
+        features = self.extract_features(query)
+        model = self.select_model(query)
+        new_scores = model.predict(features)
+        # Further logic for reranking
+
+    def extract_features(self, search_results):
+        features_list = []
+        for result in search_results:
+            features = []
+            # Elasticsearch score
+            features.append(result['_score'])
+            # Text length
+            features.append(len(result['_source']['text']))
+            # ... Other feature extraction logic
+            
+            features_list.append(features)
+        return np.array(features_list)
+
+def rate_limit_request(client_id):
+    key = f"rate_limit:{client_id}"
+    current_rate = redis.incr(key)
+    if current_rate > 10:
+        raise Exception("Rate limit exceeded")
+    redis.expire(key, 60)  # 1-minute window
+
+async def api_rerank(query: QueryModel, client_id: str):
+    rate_limit_request(client_id)
+    # Business logic here
